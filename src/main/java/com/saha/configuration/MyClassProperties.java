6643016c46fe4f8d547ba95ca3e8a6eed32641d3
Alper <Alper@Alper>
2016-01-30 13:44:45 +0200

    Son değişiklikler

---
+++ src/main/java/com/saha/configuration/MyClassProperties.java
@@ -0,0 +1,19 @@
+package com.saha.configuration;
+
+import java.util.List;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Alper
+ */
+@ConfigurationProperties(prefix = "my.class")
+@Data
+@Component
+public class MyClassProperties {
+    private Integer size;
+    private String name;
+    private String description;
+    private List<String> servers;
+}
